f2ade2f8a1461078122b0771cede780c52757724
Kai Moser <moser@glue.ch>
2021-10-08 12:50:46 +0200

    SharedPreferencesUtil: Funktion implementiert

--- Schatzkarte/app/src/main/java/ch/apprun/schatzkarte/util/SharedPreferencesUtil.java
+++ Schatzkarte/app/src/main/java/ch/apprun/schatzkarte/util/SharedPreferencesUtil.java
@@ -1,32 +1,65 @@
 package ch.apprun.schatzkarte.util;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 /**
+ * This class helps to save certain properties in the shared preferences
+ *
  * @author Kai Moser
  */
 public class SharedPreferencesUtil {
-    private final Activity activity;
+    private static final String PROPERTY_NAME_LATITUDE = "latitude";
+    private static final String PROPERTY_NAME_LONGITUDE = "longitude";
 
-    public SharedPreferencesUtil(Activity activity) {
-        this.activity = activity;
+    private final SharedPreferences sharedPref;
+
+    /**
+     * @param context Context/Activity
+     */
+    public SharedPreferencesUtil(Context context) {
+        sharedPref = context.getSharedPreferences("schatzkarte", Context.MODE_PRIVATE);
     }
 
+    /**
+     * Saves the given latitude in shared preferences
+     *
+     * @param latitude Latitude to be saved in shared preferences
+     */
     public void setLatitude(double latitude) {
-        // TODO: Implement
+        sharedPref.edit()
+                .putString(PROPERTY_NAME_LATITUDE, String.valueOf(latitude))
+                .apply();
     }
 
+    /**
+     * Retrieves Latitude from shared preferences and returns it
+     *
+     * @return Latitude from shared preferences
+     */
     public double getLatitude() {
-        // TODO: Implement
-        return 0.0;
+        String value = sharedPref.getString(PROPERTY_NAME_LATITUDE, null);
+        return Double.parseDouble(value);
     }
 
+    /**
+     * Saves the given Longitude in shared preferences
+     *
+     * @param longitude Longitude to be saved in shared preferences
+     */
     public void setLongitude(double longitude) {
-        // TODO: Implement
+        sharedPref.edit()
+                .putString(PROPERTY_NAME_LONGITUDE, String.valueOf(longitude))
+                .apply();
     }
 
+    /**
+     * Retrieves Longitude from shared preferences and returns it
+     *
+     * @return Longitude from shared preferences
+     */
     public double getLongitude() {
-        // TODO: Implement
-        return 0.0;
+        String value = sharedPref.getString(PROPERTY_NAME_LONGITUDE, null);
+        return Double.parseDouble(value);
     }
 }
