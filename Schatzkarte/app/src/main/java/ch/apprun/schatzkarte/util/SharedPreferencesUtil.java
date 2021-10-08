cf7921f1923d7aaabf4c629bbdaeb644672fb09a
Kai Moser <moser@glue.ch>
2021-10-08 10:19:32 +0200

    Grundgerüst für SharedPreferencesUtil

---
+++ Schatzkarte/app/src/main/java/ch/apprun/schatzkarte/util/SharedPreferencesUtil.java
@@ -0,0 +1,32 @@
+package ch.apprun.schatzkarte.util;
+
+import android.app.Activity;
+
+/**
+ * @author Kai Moser
+ */
+public class SharedPreferencesUtil {
+    private final Activity activity;
+
+    public SharedPreferencesUtil(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void setLatitude(double latitude) {
+        // TODO: Implement
+    }
+
+    public double getLatitude() {
+        // TODO: Implement
+        return 0.0;
+    }
+
+    public void setLongitude(double longitude) {
+        // TODO: Implement
+    }
+
+    public double getLongitude() {
+        // TODO: Implement
+        return 0.0;
+    }
+}
